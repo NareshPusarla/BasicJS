b9713efa0177a3b2e08bd7cd84f9556e51a277e4
Naresh <pnareshsatya@gmail.com>
2021-08-22 17:53:55 +0530

    Updated conversion

--- src/Conversion.js
+++ src/Conversion.js
@@ -1,8 +1,14 @@
 let feet;
+let metres;
 function inchConversion(inch){
     feet = inch/12;
     console.log(feet);
 }
 
-function()
+function rectangelePlot(length, breadth){
+    metres = (length*breadth)*0.3048;
+    console.log(metres);
+}
+
 inchConversion(42);
+rectangelePlot(60,40);
